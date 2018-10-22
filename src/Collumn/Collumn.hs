5ccec9361e370071c1435c539c023d7bee3349f5
Joris Gutjahr <joris.gutjahr@gmail.com>
2018-10-22 18:52:23 +0200

    Added Documentation with Haddock

--- src/Collumn/Collumn.hs
+++ src/Collumn/Collumn.hs
@@ -1,3 +1,13 @@
+{- |
+Module      : Collumn
+Description : Interface for structures and functions
+              for the Cortical Collumn
+Copyright   : (c) Joris Gutjahr, 2018
+License     : MIT
+Maintainer  : joris.gutjahr@gmail.com
+Stability   : experimental
+
+-}
 module Collumn
 ( LayerData,
   Layer,
@@ -7,22 +17,32 @@ module Collumn
 
 
 
-
+-- |Simple Datatype for storing things like connections  with other Layer
 data LayerData  = LayerData [[Double]] deriving (Show)
 
+-- |Layer Data stores a activationFunction that gets applied
+-- each time we want the output of a Layer on a current Collumn.
+-- As this activationFunction you can for example implement a SP.
 data Layer = Layer  {
- activationFunction :: (Collumn -> Int -> Collumn),
+ activationFunction :: (Collumn -> LayerData  -> Int -> Collumn),
  layer_data :: LayerData
 }
 
+-- |For getting the Layer to print out something on debugging
+-- I ignore the activationFunction because that would only
+-- give me "function" as output
 instance Show Layer where
   show Layer{layer_data=ld} = show ld
 
+-- | Data for many Layers where the
+-- activationFunctions are called in order of
+-- the layers list, layer_num indicates the
+-- currently executing layers index
 data Collumn = Collumn {
   layers::[Layer],
   layer_num::Int
 }
-
+-- | Add a Layer to the Collumn
 pushLayer :: Collumn -> Layer -> Collumn
 pushLayer (Collumn {layers = orgLayers,layer_num = ln}) new_layer =
   Collumn {
